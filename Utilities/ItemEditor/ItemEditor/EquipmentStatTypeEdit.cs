c48ba03d375524bdbb84644102d4d250b47d8b6a
AShottInTheDark <ashottinthedark@gmail.com>
2014-08-09 16:18:54 -0400

    Fixes to editor solution

---
+++ Utilities/ItemEditor/ItemEditor/EquipmentStatTypeEdit.cs
@@ -0,0 +1,28 @@
+﻿using GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemEditor
+{
+    public class EquipmentStatTypeEdit
+    {
+        public EquipmentStatTypes StatType { get; private set; }
+        public int Value { get; set; }
+        public string Name { get { return StatType.ToString(); } }
+
+        public EquipmentStatTypeEdit(EquipmentStatTypes statType)
+        {
+            StatType = statType;
+            Value = 10;
+        }
+
+        public EquipmentStatTypeEdit(EquipmentStatTypes statType, int value)
+        {
+            StatType = statType;
+            Value = value;
+        }
+    }
+}
---
+++ Utilities/ItemEditor/ItemEditor/EquipmentStatTypePicker.cs
@@ -0,0 +1,62 @@
+﻿using GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace ItemEditor
+{
+    public partial class EquipmentStatTypePicker : Form
+    {
+        public ObservableCollection<EquipmentStatTypeEdit> AvailableStatTypes { get; private set; }
+        public EquipmentStatTypeEdit SelectedStatType { get; set; }
+        public bool Confirmed { get; private set; }
+
+        public EquipmentStatTypePicker(ObservableCollection<EquipmentStatTypeEdit> currentStatTypes)
+        {
+            AvailableStatTypes = new ObservableCollection<EquipmentStatTypeEdit>();
+            Confirmed = false;
+
+            InitializeComponent();
+
+            foreach(EquipmentStatTypes stat in Enum.GetValues(typeof(EquipmentStatTypes)))
+            {
+                if (!currentStatTypes.Where(x => x.StatType == stat).Any())
+                {
+                    AvailableStatTypes.Add(new EquipmentStatTypeEdit(stat));
+                }
+            }
+
+            EquipmentStatTypesList.DataSource = AvailableStatTypes;
+            EquipmentStatTypesList.DisplayMember = "Name";
+
+            if(AvailableStatTypes.Count > 0)
+                SelectedStatType = AvailableStatTypes[0];
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            if (SelectedStatType != null)
+                Confirmed = true;
+
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EquipmentStatTypesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedStatType = (EquipmentStatTypeEdit)((ListBox)sender).SelectedItem;
+        }        
+    }
+}
